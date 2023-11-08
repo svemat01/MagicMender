5246c286dcae21425a37b3ee6bd53641076d9344
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-11-08 12:55:45 +0100

    fix walking animations

---
+++ Assets/ClintMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClintMovement : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float moveSpeed = 2.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            animator.SetFloat("Speed4", moveSpeed);
+            Vector3 v = transform.up * moveSpeed;  // -transform.right = left
+            GetComponent<Rigidbody2D>().velocity = v;  // Sets velocity to left movement
+        }
+        else if (Input.GetKey("down"))
+        {
+            animator.SetFloat("Speed2", moveSpeed);
+            Vector3 v = -transform.up * moveSpeed;  // -transform.right = left
+            GetComponent<Rigidbody2D>().velocity = v;  // Sets velocity to left movement
+        }
+        else if (Input.GetKey("left"))
+        {
+            animator.SetFloat("Speed3", moveSpeed);
+            Vector3 v = -transform.right * moveSpeed;  // -transform.right = left
+            GetComponent<Rigidbody2D>().velocity = v;  // Sets velocity to left movement
+        }
+        else if (Input.GetKey("right"))
+        {
+            animator.SetFloat("Speed", moveSpeed);
+            Vector3 v = transform.right * moveSpeed;  // -transform.right = left
+            GetComponent<Rigidbody2D>().velocity = v;  // Sets velocity to left movement
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+            animator.SetFloat("Speed2", 0);
+            animator.SetFloat("Speed3", 0);
+            animator.SetFloat("Speed4", 0);
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+    }
+}
