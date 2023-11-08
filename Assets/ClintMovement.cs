ae110805332ddaf236ec7a74a8e42cc5b55bddfa
zimonjohnsson <142591467+zimonjohnsson@users.noreply.github.com>
2023-11-08 14:25:34 +0100

    hammering (#11)

--- Assets/ClintMovement.cs
+++ Assets/ClintMovement.cs
@@ -40,12 +40,19 @@ public class ClintMovement : MonoBehaviour
             Vector3 v = transform.right * moveSpeed;  // -transform.right = left
             GetComponent<Rigidbody2D>().velocity = v;  // Sets velocity to left movement
         }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Hammer", true);
+            moveSpeed = 0f;
+        }
         else
         {
+            moveSpeed = 2.0f;
             animator.SetFloat("Speed", 0);
             animator.SetFloat("Speed2", 0);
             animator.SetFloat("Speed3", 0);
             animator.SetFloat("Speed4", 0);
+            animator.SetBool("Hammer", false);
             GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         }
     }
