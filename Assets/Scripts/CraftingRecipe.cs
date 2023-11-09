207ff9db982ef0408c169fe9226971778e348b43
Salah.Alhassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-11-09 14:39:58 +0100

    Crafting system
    Some bugs but working probably

---
+++ Assets/Scripts/CraftingRecipe.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RECIPE", menuName = "Crafting Recipe", order = 0)]
+public class CraftingRecipe : ScriptableObject
+{
+    public string id;
+    public InventoryItemData[] ingredients;
+    public InventoryItemData[] results;
+}
