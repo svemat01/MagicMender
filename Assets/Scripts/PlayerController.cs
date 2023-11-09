db5f68d927aebc0e4f519bcd70f001d67845a990
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-11-09 10:10:45 +0100

    Improve Player Movement and Animator

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerItemAnimation
+{
+    None, // 0
+    Hammer, // 1
+}
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    public float moveSpeed = 5f;
+
+    public Animator animator;
+    
+    private Vector2 movement;
+    
+    public PlayerItemAnimation Item = PlayerItemAnimation.None;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        movement = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // use input to set movement vector
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+        // set animator parameters
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+    }
+    
+    private void FixedUpdate()
+    {
+        // move the player
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+    
+    public void EquipItem(PlayerItemAnimation Item)
+    {
+        animator.SetInteger("Item", (int)Item);
+    }
+}
