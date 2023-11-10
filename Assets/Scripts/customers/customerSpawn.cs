b1a520dddcc6b60563008737ed46f77c69a4c187
Jonte88 <jonathan.thj15@gmail.com>
2023-11-10 09:06:48 +0100

    Remove Lag-creator and destroying customers

--- Assets/Scripts/customers/customerSpawn.cs
+++ Assets/Scripts/customers/customerSpawn.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
-using static UnityEditor.Progress;
 
 public class customerSpawn : MonoBehaviour
 {
-
     public GameObject CustomerPrefab;
-
     public int currentCustomer = 0;
+    private GameObject currentCustomerGameObject;
+    private GameObject currentCustomerObject;
 
     // Use this for initialization
     void Start()
@@ -20,31 +19,34 @@ public class customerSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        // Add any update logic if needed
     }
+
     public void CreateCustomer()
     {
         System.Random randomCustomer = new System.Random();
-
-        var customer = Instantiate(CustomerPrefab, new Vector3(0.5f, 1, 0), Quaternion.identity);
-        var spriteRenderer = customer.GetComponent<SpriteRenderer>();
-
+        currentCustomerObject = Instantiate(CustomerPrefab, new Vector3(0.5f, 0.5f, 0), Quaternion.identity);
+        var spriteRenderer = currentCustomerObject.GetComponent<SpriteRenderer>();
         var sprite = Resources.Load<Sprite>("Sprites/Customers/customer" + (randomCustomer.Next(3) + 1));
         spriteRenderer.sprite = sprite;
 
-        var cSCript = customer.GetComponent<CustomerScript>();
+        var cSCript = currentCustomerObject.GetComponent<CustomerScript>();
         cSCript.spawner = this;
         Debug.Log("Sprite Created");
     }
 
     public void CustomerClick()
     {
-        if (currentCustomer == 0)
+        if (currentCustomerObject == null)
         {
             CreateCustomer();
-            currentCustomer = 1;
-        } else {
-
+        }
+        else
+        {
+            Debug.Log("Customer GameObject found");
+            Destroy(currentCustomerObject);
+            currentCustomerObject = null;
+            CreateCustomer();
         }
         Debug.Log("Clicked");
     }
