3079361be720662e80c497fcd178a806e0dd62c6
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-11-13 12:31:32 +0100

    Fix ordering

--- Assets/Scripts/customers/customerSpawn.cs
+++ Assets/Scripts/customers/customerSpawn.cs
@@ -4,33 +4,35 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
-public class customerSpawn : MonoBehaviour
+public class CustomerSpawn : MonoBehaviour
 {
     public GameObject CustomerPrefab;
     public int currentCustomer = 0;
     private GameObject currentCustomerObject;
     public GameObject CustomerBubble;
     private GameObject currentCustomerBubble;
-    public GameObject ItemPreview;
-    public InventoryItemData data;
+    // public GameObject ItemPreview;
+    // public InventoryItemData data;
     
-    ItemPreview scriptName; // local variable to script instance in this object
+    // ItemPreview scriptName; // local variable to script instance in this object
 
     public float MoveSpeed = 10f;
 
-    string[][] orders;
+    // string[][] orders;
 
+    public InventoryItemData[] Orders;
+    
     // Use this for initialization
     void Start()
     {
-        orders = new string[][]
-        {
-            new string[] { "order1", "ironsword" },
-            new string[] { "order2", "ironswordmagical" }
-        };
+        // orders = new string[][]
+        // {
+        //     new string[] { "order1", "ironsword" },
+        //     new string[] { "order2", "ironswordmagical" }
+        // };
          
         CustomerClick();
-        scriptName = gameObject.GetComponent<UpdateItem>(IronItem, 1);
+        // scriptName = gameObject.GetComponent<UpdateItem>(IronItem, 1);
     }
 
     // Update is called once per frame
@@ -51,25 +53,30 @@ public class customerSpawn : MonoBehaviour
         var cSCript = currentCustomerObject.GetComponent<CustomerScript>();
         cSCript.spawner = this;
         Debug.Log("Sprite Created");
+        
+        // Take random order from orders lsit
+        var order = Orders[randomCustomer.Next(Orders.Length)];
+        
+        cSCript.SetOrder(new []{order});
 
-        if (CustomerBubble != null)
-        {
-            
-            // Instantiate the bubble as a child of the customer sprite
-            /*currentCustomerBubble = Instantiate(CustomerBubble, currentCustomerObject.transform);
-            currentCustomerBubble.transform.localPosition = new Vector3(0, 0, 0);
-
-            var bubbleRenderer = currentCustomerBubble.GetComponent<SpriteRenderer>();
-            var bubbleSprite = Resources.Load<Sprite>("Orders/" + orders[1][1]);
-            Debug.Log(orders[1][1]);
-            bubbleRenderer.sprite = bubbleSprite;*/
-
-            Debug.Log("Bubble Created");
-        }
-        else
-        {
-            Debug.LogError("CustomerBubble is not assigned in the Inspector.");
-        }
+//         if (CustomerBubble != null)
+//         {
+//             
+//             // Instantiate the bubble as a child of the customer sprite
+//             /*currentCustomerBubble = Instantiate(CustomerBubble, currentCustomerObject.transform);
+//             currentCustomerBubble.transform.localPosition = new Vector3(0, 0, 0);
+//
+//             var bubbleRenderer = currentCustomerBubble.GetComponent<SpriteRenderer>();
+//             var bubbleSprite = Resources.Load<Sprite>("Orders/" + orders[1][1]);
+//             Debug.Log(orders[1][1]);
+//             bubbleRenderer.sprite = bubbleSprite;*/
+//
+//             Debug.Log("Bubble Created");
+//         }
+//         else
+//         {
+//             Debug.LogError("CustomerBubble is not assigned in the Inspector.");
+//         }
     }
 
 
