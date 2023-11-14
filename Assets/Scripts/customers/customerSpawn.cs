91c77c360cb82b875e3b3f9dc58fafc5e84de1a8
zimonjohnsson <142591467+zimonjohnsson@users.noreply.github.com>
2023-11-14 09:56:01 +0100

    Auto stash before merge of "master" and "origin/master" (#35)

--- Assets/Scripts/customers/customerSpawn.cs
+++ Assets/Scripts/customers/customerSpawn.cs
@@ -31,9 +31,12 @@ public class CustomerSpawn : MonoBehaviour
         //     new string[] { "order1", "ironsword" },
         //     new string[] { "order2", "ironswordmagical" }
         // };
-         
-        CustomerClick();
+
+        // CustomerClick();
         // scriptName = gameObject.GetComponent<UpdateItem>(IronItem, 1);
+        Debug.Log("yeet");
+        Debug.Log(this);
+        CreateCustomer();
     }
 
     // Update is called once per frame
@@ -52,6 +55,7 @@ public class CustomerSpawn : MonoBehaviour
         spriteRenderer.sprite = sprite;
 
         var cSCript = currentCustomerObject.GetComponent<CustomerScript>();
+        Debug.Log(cSCript);
         cSCript.spawner = this;
         Debug.Log("Sprite Created");
         
