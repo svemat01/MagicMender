dbee5bf5d62b3c7856050d416fec3b6f94d56129
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-11-14 14:59:06 +0100

    removed destroy from pickup script

--- Assets/Scripts/InventorySystem/ItemPickup.cs
+++ Assets/Scripts/InventorySystem/ItemPickup.cs
@@ -19,7 +19,7 @@ public class ItemPickup : MonoBehaviour
         
         if (HotbarController.Instance.AddItem(itemData))
         {
-            Destroy(gameObject);
+            
         }
     }
 }
