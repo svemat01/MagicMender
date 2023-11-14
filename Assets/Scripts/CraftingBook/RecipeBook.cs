a58a7d1fbf0003fee219dc8c323216caf6155c75
Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>
2023-11-14 15:20:35 +0100

    Create Book and rcipe pages

---
+++ Assets/Scripts/CraftingBook/RecipeBook.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RecipeBook : MonoBehaviour
+{
+    public GameObject recipePagePrefab;
+    public Transform recipePagesContainer;
+    public GameObject buttonContainer;
+    public CraftingRecipe[] recipes;
+
+    public int currentPageIndex = 0;
+
+    void Awake()
+    {
+        recipes = Resources.LoadAll<CraftingRecipe>("Recipes");
+    
+    }
+
+    void Start()
+    {
+        recipePagesContainer.gameObject.SetActive(false);
+        buttonContainer.SetActive(false);
+        if (recipes != null && recipes.Length > 0)
+        {
+            CreateRecipePages();
+            ShowCurrentPages();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("R pressed");
+            ToggleRecipeBook();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            LeftPage();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RightPage();
+        }
+
+        // You can add code here to handle page navigation, e.g., using arrow keys.
+    }
+
+
+    public void RightPage()
+
+    {
+        currentPageIndex += 2;
+        ShowCurrentPages();
+    }
+
+    public void LeftPage()
+    {
+        currentPageIndex -= 2;
+        ShowCurrentPages();
+    }
+
+
+    void CreateRecipePages()
+    {
+        for (int i = 0; i < recipes.Length; i += 2)
+        {
+            GameObject recipePage1 = Instantiate(recipePagePrefab, recipePagesContainer);
+            recipePage1.SetActive(false);
+
+            RecipePage pageScript1 = recipePage1.GetComponent<RecipePage>();
+            if (pageScript1 != null)
+            {
+                pageScript1.SetRecipe(recipes[i]);
+            }
+
+            if (i + 1 < recipes.Length)
+            {
+                GameObject recipePage2 = Instantiate(recipePagePrefab, recipePagesContainer);
+                recipePage2.SetActive(false);
+
+                RecipePage pageScript2 = recipePage2.GetComponent<RecipePage>();
+                if (pageScript2 != null)
+                {
+                    pageScript2.SetRecipe(recipes[i + 1]);
+                }
+                else
+                {
+                    Debug.LogError("pageScript2 is null");
+                }
+
+                // Move the second recipe page to the right of the first one
+                RectTransform rectTransform1 = recipePage1.GetComponent<RectTransform>();
+                RectTransform rectTransform2 = recipePage2.GetComponent<RectTransform>();
+                rectTransform2.anchoredPosition = new Vector2(rectTransform1.anchoredPosition.x + rectTransform1.rect.width, rectTransform1.anchoredPosition.y);
+            }
+        }
+    }
+
+    void ShowCurrentPages()
+    {
+        int startIndex = Mathf.Clamp(currentPageIndex, 0, recipePagesContainer.childCount - 2);
+
+        for (int i = 0; i < recipePagesContainer.childCount; i++)
+        {
+            recipePagesContainer.GetChild(i).gameObject.SetActive(i >= startIndex && i < startIndex + 2);
+        }
+    }
+
+    void ToggleRecipeBook()
+    {
+
+         buttonContainer.SetActive(!buttonContainer.activeSelf);
+        recipePagesContainer.gameObject.SetActive(!recipePagesContainer.gameObject.activeSelf);
+    }
+}
---
+++ Assets/Scripts/CraftingBook/RecipePage.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class RecipePage : MonoBehaviour
+{
+    public Transform ingredientContainer;
+    public Transform resultContainer;
+    public GameObject imagePrefab; // Assign your Image UI prefab here
+
+    public Text pagenumber;
+    public void SetRecipe(CraftingRecipe recipe)
+    {
+        if (recipe != null)
+        {
+            UpdateRecipeImages(recipe.ingredients, ingredientContainer);
+            UpdateRecipeImages(recipe.results, resultContainer);
+            pagenumber.text = recipe.id.ToString(); // Add this line to display the recipe number as the page number
+
+        }
+    }
+
+
+    void UpdateRecipeImages(InventoryItemData[] items, Transform container)
+    {
+
+        if (items != null && container != null)
+        {
+            // Clear existing children
+            foreach (Transform child in container)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // Instantiate and set up images with a GridLayoutGroup
+            GridLayoutGroup gridLayout = container.gameObject.AddComponent<GridLayoutGroup>();
+            gridLayout.cellSize = new Vector2(75f, 75f); // Set the size of each cell
+            gridLayout.spacing = new Vector2(175f, 175f); // Set the spacing between cells
+            gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount; // Set the layout constraint to a fixed number of columns
+            gridLayout.constraintCount = 3; // Set the number of columns to 4
+            gridLayout.childAlignment = TextAnchor.MiddleCenter; // Set the alignment of the children to the center of the cell
+
+            // Instantiate and set up images with a HorizontalLayoutGroup
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] != null)
+                {
+                    GameObject imageObject = Instantiate(imagePrefab, container);
+                    Image image = imageObject.GetComponent<Image>();
+                    image.sprite = items[i].icon;
+
+                }
+            }
+        }
+    }
+}
