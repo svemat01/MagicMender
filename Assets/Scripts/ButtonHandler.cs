f50dc0909c3497bda1327b99aa0bf583efdaa81f
Salah.Alhassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-11-15 09:16:52 +0100

    Menu UI (#40)

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void StartGame() // Renamed from Start to StartGame
+    {
+        // Load the game scene
+        SceneManager.LoadScene("clintidle");
+    }
+
+    public void OpenOptions()
+    {
+        // Load the options scene
+        SceneManager.LoadScene("OptionsScene");
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
